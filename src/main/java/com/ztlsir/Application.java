2b4274ec66b7c0a879510a24dc0fce5eacfcaf3f
ztlsir <584821699@qq.com>
2020-05-31 13:01:03 +0800

    get config from config server

--- src/main/java/com/ztlsir/Application.java
+++ src/main/java/com/ztlsir/Application.java
@@ -2,12 +2,14 @@ package com.ztlsir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
+@RefreshScope
 public class Application {
 
 	public static void main(String[] args) {
